af788f191a2816bebe508188035b76ae32724075
guykamty <guykamty@gmail.com>
2025-09-26 15:48:15 +0700

    done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express from "express";
 import morgan from 'morgan';
+import type { Request, Response } from "express";
+import router from "./routes/courseRoutes.js";
 
 const app: any = express();
 
 //Middleware
 app.use(express.json());
 app.use(morgan('dev'));
+
+
+app.get("/",(req: Request, res: Response) => {
+    return res.status(200).json({
+	success: true,
+	message: "lab 15 API service successfully"
+});
+});
+
+app.get("/me",(req: Request, res: Response) => {
+    return res.status(200).json({
+	success : true,
+	message : "Student Information",
+	data : {
+		studentId : "670612116",
+		firstName : "Kunamorn",
+		lastName : "Tailumyong",
+		program : "CPE",
+		section : "801"
+        }
+    });
+});
+
+app.use("/api/v2", router);
 
 app.listen(3000, () =>
   console.log("🚀 Server running on http://localhost:3000")
